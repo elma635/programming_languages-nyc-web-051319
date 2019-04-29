460879ee156f8f0233425506701174942a8bdcff
Elma Rahman <elmarahman635@gmail.com>
2019-04-29 02:12:41 +0000

    Automatically backed up by Learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -2,19 +2,20 @@ require 'pry'
 
 
 def reformat_languages(languages)
-  new_hash = {}
-    languages.each do |language_name, language_attr|
-        language_attr.each do |type, type_value|
-          new_hash[:language_attr] = "ruby"
+  new_hash = {} #:language => {:type => , :style => }
+    #new_hash[:language] = {type: " ", style:" "}
+      languages.each do |language_style, language_hash|
+        language_hash.each do |type_name, type_value|
+          binding.pry
+          new_hash[type_name][:type] = []
 
+            new_hash[type_name][:type] << language_style
 
-
-    #new_hash[:language] = "value"
-    #new_hash[:language] = {attribute: "info"}
-
-      end
-    end
+          end
+        end
+    new_hash
   end
 
 
-#you have to iterate  through the old hash and reformat it to  a new hash 
+#you have to iterate  through the old hash and reformat it to  a new hash
+#-- build a new hash that keys = :languages values => {:type => and :style => }
