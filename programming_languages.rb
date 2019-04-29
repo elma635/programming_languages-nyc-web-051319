31d010e3087246395e290627a47c88fc16f64ffa
Elma Rahman <elmarahman635@gmail.com>
2019-04-29 02:33:34 +0000

    Automatically backed up by Learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -14,10 +14,10 @@ def reformat_languages(languages)
             new_hash[language][:style] <<  oo_or_functional
             if new_hash[language][attribute].nil?
               new_hash[language][attribute] = str_value
+            end
           end
         end
       end
-    end
     new_hash
   end
 
